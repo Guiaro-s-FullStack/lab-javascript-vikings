ad67143b34564b918957e2a545571feadf92480b
Gabriel Sicuto <gabrielsicuto@gmail.com>
2023-06-20 20:25:10 -0300

    Interation 1 - Done

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,18 @@
 // Soldier
 class Soldier {
-  constructor(health, strength) {}
+  constructor(health, strength) {
+    this.strength = strength
+    this.health = health
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(theDamage) {
+    // this.health = this.health - theDamage
+    this.health -= theDamage
+  }
 }
 
 // Viking
